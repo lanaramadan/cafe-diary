f353c48534cde5bff687b61be901ba9e003b24d4
Lana Ramadan <lanamramadan@gmail.com>
2024-08-27 00:52:56 -0700

    Search bar styling

--- components/CafeSearch.tsx
+++ components/CafeSearch.tsx
@@ -11,6 +11,8 @@ import {
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { Colors } from '@/constants/Colors';
 import { CafeData } from './types/CafeData';
+import { Ionicons } from '@expo/vector-icons';
+import { ThemedText } from './ThemedText';
 
 interface CafeSearchProps {
   onCafeSelect: (CafeData: CafeData) => void;
@@ -29,6 +31,15 @@ export const CafeSearch: React.FC<CafeSearchProps> = ({ onCafeSelect }) => {
       placeholder='Search cafés'
       GooglePlacesDetailsQuery={{ fields: "formatted_address,geometry" }}
       fetchDetails={true}
+      renderRow={(data) => (
+        <View style={styles.searchBar.row}>
+          <Ionicons name="location-outline" size={27} color={Colors['light'].text} />
+          <View style={styles.resultContainer}>
+            <Text style={styles.name}>{data.structured_formatting.main_text}</Text>
+            <Text style={styles.address}>{data.structured_formatting.secondary_text}</Text>
+          </View>
+        </View>
+      )}
       onPress={(data, details = null) => {
         const CafeData: CafeData = {
           name: data?.structured_formatting?.main_text || '',
@@ -47,32 +58,64 @@ export const CafeSearch: React.FC<CafeSearchProps> = ({ onCafeSelect }) => {
 };
 
 const styles = StyleSheet.create({
+  resultContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: Colors['light'].text,
+    width: 315
+  },
+  address: {
+    fontSize: 12,
+    textAlign: 'left',
+    color: Colors['light'].text,
+    width: 315
+  },
   searchBar: {
     textInput: {
+      height: 40,
       backgroundColor: Colors['light'].background,
-      borderRadius: 10,
-      borderWidth: 2,
       borderColor: Colors['light'].primaryGreen,
-      height: 45,
+      borderWidth: 2,
+      borderRadius: 10,
       color: Colors['light'].text,
       fontSize: 16,
     },
-    predefinedPlacesDescription: {
-      color: '#1faadb',
-    },
-    poweredContainer: {
-      borderColor: Colors['light'].primaryGreen,
-      borderTopWidth: 1,
+    listView: {
+      paddingTop: 5,
+      paddingBottom: 5,
+      marginTop: 5,
+      marginBottom: 5,
+      borderRadius: 10,
+      borderWidth: 2,
+      borderColor: Colors['light'].secondaryGreen,
     },
     row: {
-      backgroundColor: Colors['light'].secondaryGreen,
-      padding: 13,
-      height: 35,
+      // backgroundColor: Colors['light'].secondaryGreen,
+      backgroundColor: Colors['light'].background,
+      paddingLeft: 5,
+      paddingRight: 5,
+      paddingTop: 2,
+      paddingBottom: 2,
       flexDirection: 'row',
+      alignItems: 'center',
+      gap: 5,
     },
     separator: {
-      height: 1,
-      backgroundColor: Colors['light'].primaryGreen,
+      height: 1.5,
+      backgroundColor: Colors['light'].secondaryGreen,
+      margin: 10,
+    },
+    poweredContainer: {
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    powered: {
+      width: 20,
+      height: 20,
     },
   },
 });
